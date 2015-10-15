aef5dc13e643de892ba2ff01c0a6f0551a195bcb
gse1864 <emilysgoddard@gmail.com>
2015-10-14 20:23:14 -0600

    'Add Quote' functioning

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,11 @@
 var app = angular.module("quoteBook");
 
-app.controller("mainCtrl", ["$scope", function($scope) {
-    
+app.controller("mainCtrl", ["$scope", "dataService", function($scope, dataService) {
+                            
+    $scope.quotes = dataService.getData();
+    $scope.addData = function(userInput) {
+        dataService.addData(userInput);
+        $scope.quotes = dataService.getData();
+    }
     
 }]); //end
